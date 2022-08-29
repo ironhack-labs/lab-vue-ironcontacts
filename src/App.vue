0fed6a1a54019684ebb179cc0b610342b3fe3971
Irina <irinakhr@mail.ru>
2022-08-29 23:01:30 +0200

    done but not sure

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
     <h1 class="title">Iron Contacts</h1>
+    <article class="contactbuttons">
+      <button @click="addrandom">Add Random Contact</button>
+      <button @click="mostpopular">Sort by Popularity</button>
+      <button @click="alphabeticalorder">Sort by Name</button>
+    </article>
+    <br />
     <table class="contactstable">
       <tr>
         <th>Picture</th>
@@ -8,6 +14,7 @@
         <th>Popularity</th>
         <th>Won Oscar</th>
         <th>Won Emmy</th>
+        <th>Actions</th>
       </tr>
       <tr v-for="contact in fivecontacts" :key="contact.id">
         <td><img class="picture" :src="contact.pictureUrl" /></td>
@@ -17,10 +24,11 @@
         <td>
           <p>{{ contact.popularity }}</p>
         </td>
-        <td v-if="contact.wonOscar">
-          <img src="./src/assets/logo.png" />
-        </td>
-        <td v-if="contact.wonEmmy"><img src="./src/assets/logo.png" /></td>
+        <td v-if="contact.wonOscar">Yes!!</td>
+        <td v-else></td>
+        <td v-if="contact.wonEmmy">Yes!!</td>
+        <td v-else></td>
+        <td><button @click="removecontact(contact)">Delete</button></td>
       </tr>
     </table>
   </div>
@@ -37,6 +45,39 @@ export default {
     return {
       fivecontacts,
     };
+  },
+  methods: {
+    addrandom() {
+      let randomcontactlist = contacts.splice(6, 5);
+      this.fivecontacts.unshift(randomcontactlist[0]);
+    },
+    mostpopular() {
+      let popular = this.fivecontacts.sort((a, b) => {
+        if (a.popularity > b.popularity) {
+          return -1;
+        } else if (a.popularity < b.popularity) {
+          return 1;
+        } else {
+          return 0;
+        }
+        return { popular };
+      });
+    },
+    alphabeticalorder() {
+      let orderbyname = this.fivecontacts.sort((a, b) => {
+        if (a.name < b.name) {
+          return -1;
+        } else if (a.name > b.name) {
+          return 1;
+        } else {
+          return 0;
+        }
+        return { orderbyname };
+      });
+    },
+    removecontact(contact) {
+      this.fivecontacts = this.fivecontacts.filter((t) => t !== contact);
+    },
   },
 };
 </script>

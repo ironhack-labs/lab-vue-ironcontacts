a1f1016dd490b27d3db3d74459c858e1c13d428f
andayanda <carmelaespinola@gmail.com>
2022-09-01 19:40:31 +0200

    iteration1 no se ven las imagenes

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,43 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import contactData from "./contacts.json";
+export default {
+  name: "App",
+  components: {},
+  data() {
+    const contacts = contactData.slice(0, 5);
+    return {
+      contacts: contacts,
+    };
+  },
+  computed: {},
+};
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <h1>IronContacts</h1>
+  <div>
+    <table style="width: 100%">
+      <thead>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+        </tr>
+      </thead>
+      <tbody v-for="item in contacts" :key="item.id">
+        <td>
+          <img
+            :src="item.pictureUrl"
+            width="100px"
+            height="120px"
+            alt="contact Image"
+          />
+        </td>
+        <td>{{ item.name }}</td>
+        <td>{{ item.popularity.toFixed(2) }}</td>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <style>

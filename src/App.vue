8e6696e7d9e8a1b23d718a83b8c65bb5f7e50a66
Txell <meritxell@gold.barcelona>
2022-09-01 19:50:38 +0200

    array contacts

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,50 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div>
+    <h1>Iron Contacts</h1>
+    <table class="table">
+      <thead>
+        <td><b>Picture</b></td>
+        <td><b>Name</b></td>
+        <td><b>Popularity</b></td>
+      </thead>
+      <tbody v-for="fivecontact in fivecontacts" :key="fivecontact">
+        <td><img :src="`${ fivecontact.pictureUrl }`" /></td>
+        <td> {{ fivecontact.name }} </td>
+        <td> {{ fivecontact.popularity }} </td>
+      </tbody>
+    </table>
+  </div>
 </template>
+
+<script>
+import contacts from "./contacts.json";
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      contacts: contacts,
+      fivecontacts: [],
+    }  
+  },
+  created() {
+    let fiveContacts = contacts.slice(0, 5).push(fivecontacts);
+    return {
+      fiveContacts
+    };
+  },
+  // methods: {
+  //   addRandom() {
+  //     let randomContactList = contacts.splice(5, contacts.lenght-1);
+  //     this.fiveContacts.unshift(randomContactList[0]);
+  //   },
+  //   mostPopular() {
+  //     let popular = this.fiveContacts.sort((a,b) => a.popularity < b.popularity);
+  //     return popular;
+  //   },
+  // },
+};
+</script>
 
 <style>
 #app {
@@ -17,5 +54,15 @@ import HelloWorld from './components/HelloWorld.vue'
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.table > thead > td {
+  padding-left: 160px;
+}
+.table > tbody > td {
+  padding-left: 150px;
+}
+img {
+  height: 185px;
+  width: 130px;
 }
 </style>

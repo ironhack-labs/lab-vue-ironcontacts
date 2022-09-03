90d282d06aca14663ee7399e7c2bb7103681afed
Txell <meritxell@gold.barcelona>
2022-09-03 10:19:56 +0200

    iteration 4 and fixed iterations

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <h1>Iron Contacts</h1>
-    <button @click="addRandom()">Add a Random Contact</button>
+    <button @click="getRandomElem">Add a Random Contact</button>
+    <button @click="mostPopular">Sort by popularity</button>
+    <button @click="alphabetic">Sort by name</button>
     <table class="table">
       <thead>
         <td><b>Picture</b></td>
@@ -9,14 +11,16 @@
         <td><b>Popularity</b></td>
         <td><b>Won an Oscar</b></td>
         <td><b>Won an Emmy</b></td>
+        <td><b>Actions</b></td>
       </thead>
       <tbody>
-        <tr v-for="contact in contacts" :key="contact">
-          <td><img :src="`${ contact.pictureUrl }`" /></td>
+        <tr v-for="contact in fiveContacts" :key="contact.id">
+          <td><img :src="`${ contact.pictureUrl }`" class="actor-img"/></td>
           <td> {{ contact.name }} </td>
           <td> {{ contact.popularity }} </td>
-          <td><img v-if=" contact.wonOscar " src="../src/assets/trophy-solid-24.png" alt="trophy"></td>
-          <td><img v-if=" contact.wonEmmy " src="../src/assets/trophy-solid-24.png" alt="trophy"></td>
+          <td><img v-if=" contact.wonOscar " src="../src/assets/icons8-trophy-48.png" alt="trophy"></td>
+          <td><img v-if=" contact.wonEmmy " src="../src/assets/icons8-trophy-48.png" alt="trophy"></td>
+          <td><button>Delete</button></td>
         </tr>
       </tbody>
     </table>
@@ -29,20 +33,41 @@ import contactsData from "./contacts.json";
 export default {
   name: 'App',
   data() {
-    let contacts = contactsData.slice(0, 5);
     return {
-      contacts: contacts,
-    }  
+      contacts: contactsData,
+      fiveContacts: [],
+    }
+  },
+  created() {
+    for (let i = 0; i < 5; i++) {
+      this.fiveContacts.push(this.contacts.shift())
+    }
   },
   methods: {
-    addRandom() {
-      let randomContactList = contactsData.splice(5, contactsData.length-1);
-      this.contacts.unshift(randomContactList[0]);
+    getRandomInt(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min) + min);
     },
-    // mostPopular() {
-    //   let popular = this.contacts.sort((a,b) => a.popularity < b.popularity);
-    //   return popular;
-    // },
+    getRandomElem() {
+      if (this.contacts.length) {
+        const randomPos = this.getRandomInt(0, this.contacts.length - 1);
+        const randomElem = this.contacts[randomPos];
+        const elemToShow = this.fiveContacts.findIndex(elem => elem === randomElem);
+
+        if (elemToShow === -1) {
+          this.fiveContacts.push(randomElem);
+          const elemToRemoveIndex = this.contacts.findIndex(elem => elem === randomElem);
+          this.contacts.splice(elemToRemoveIndex, 1);
+        }
+      }
+    },
+    mostPopular() {
+      this.fiveContacts.sort((a,b) => a.popularity < b.popularity ? 1 : -1);
+    },
+    alphabetic() {
+      this.fiveContacts.sort((a,b) => a.name > b.name ? 1 : -1);
+    },
   },
 };
 </script>
@@ -57,15 +82,12 @@ export default {
   margin-top: 60px;
 }
 .table > thead > td {
-  padding-left: 130px;
+  padding-left: 60px;
 }
-/* .table > tbody > tr {
-  width: 90%;
-} */
 .table > tbody > tr > td {
-  padding-left: 130px;
+  padding-left: 30px;
 }
-img {
+.actor-img {
   height: 185px;
   width: 130px;
 }

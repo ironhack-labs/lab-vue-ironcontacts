24c6240b46adbeccccacdba61f3d20910c0dfb63
Aberinkula36 <jmsaezteruel@hotmail.com>
2022-09-10 13:21:12 +0200

    lab completed

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="container" v-if="contacts.length">
-        <button v-on:click="randomize">Add random contact</button>
-        <h1>IronContacts</h1>
+
+        <h1 style="font-size: 50px;">IronContacts</h1>
+        <button class="button" v-on:click="randomize">Add random contact</button>
+        <button class="button" v-on:click="popSort">Sort by popularity</button>
+        <button class="button" v-on:click="nameSort">Sort by name</button>
         <div class="contacts">
             <table style="width: 1000px">
                 <tr class="tabla">
@@ -20,12 +23,15 @@
                     <th>
                         <h1>Won Emmy</h1>
                     </th>
+                    <th>
+                        <h1>Actions</h1>
+                    </th>
                 </tr>
             </table>
         </div>
     </div>
-    <div class="contact" v-for="contact in filteredContacts" :key="contact">
-        <router-link to="/">
+    <div class="contact" v-for="(contact, index) in filteredContacts" :key="contact">
+        <router-link style="text-decoration: none;" to="/">
             <div class="contacts">
                 <table style="width: 1000px">
                     <tr class="tabla">
@@ -34,7 +40,7 @@
                             <p>{{ contact.name }}</p>
                         </td>
                         <td>
-                            <p>{{ contact.popularity }}</p>
+                            <p>{{ (contact.popularity).toFixed(2) }}</p>
                         </td>
                         <td v-if="contact.wonOscar">
                             <p>🏆</p>
@@ -43,10 +49,13 @@
                             <p> </p>
                         </td>
                         <td v-if="contact.wonEmmy">
-                            <p>🏆</p>
+                            <p>⭐</p>
                         </td>
                         <td v-else>
                             <p> </p>
+                        </td>
+                        <td>
+                            <button class="button" v-on:click="idDelete(index)">Delete</button>
                         </td>
                     </tr>
                 </table>
@@ -72,25 +81,43 @@ export default {
     methods: {
         randomize: function () {
             this.filteredContacts.push(this.contacts[Math.floor(Math.random() * this.contacts.length)]);
+        },
+        popSort: function () {
+            this.filteredContacts.sort((a, b) => b.popularity - a.popularity);
+        },
+        nameSort: function () {
+            this.filteredContacts.sort((a, b) => {
+                if (a.name > b.name) return 1;
+                else return -1;
+            })
+        },
+        idDelete: function (index) {
+            this.filteredContacts.splice(index, 1);
         }
     }
 };
 </script>
 
 <style>
+.contacts {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+}
+
 .tabla {
     display: flex;
     justify-content: space-around;
 }
 
 th {
-    margin-left: 50px;
+    width: 200px;
 }
 
-.contacts {
-    display: flex;
-    flex-direction: column;
-    margin-left: 20px;
+td {
+    width: 200px;
+    align-self: center;
 }
 
 p,
@@ -98,7 +125,16 @@ h1 {
     color: black;
 }
 
+p {
+    font-size: 25px;
+}
+
 img {
-    width: 120px;
+    width: 100px;
+}
+
+.button {
+    font-size: 20px;
+    margin: 10px;
 }
 </style>
